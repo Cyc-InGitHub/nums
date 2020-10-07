1f7a525a88bf6eb2357410329e20bc6a2577fbaf
xiaozhong123 <1318289215@qq.com>
2020-10-07 18:25:58 +0800

    zky

---
+++ src/main/Middle.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Middle {
+
+	public static List Integer(List<java.lang.Integer> a) {
+		Collections.sort(a);
+		System.out.println("中值为"+a.get(a.size() / 2));
+		return a;
+	}
+}
